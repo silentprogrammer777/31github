03defadb76b10e2b0f312b2d781549ac7f53f3e7
vkusnyipecheniya <150159157+vkusnyipecheniya@users.noreply.github.com>
2023-11-12 21:30:53 +0600

    Add files via upload
    mmm

--- src/App.js
+++ src/App.js
@@ -1,17 +1,24 @@
-import React, {useState} from "react";
+import logo from './logo.svg';
+import './App.css';
+
 function App() {
-    const [data,setData] = useState({})
   return (
-   <div>
-     <h3></h3>
-       <input value={data.name} onChange={e=>
-       setData({...data,name:e.target.value})}
-       placeholder='Enter name'/>
-       {/*<div>*/}
-       {/*    <b>Valur:</b>*/}
-       {/*    {name}*/}
-       {/*</div>*/}
-   </div>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 }
 
