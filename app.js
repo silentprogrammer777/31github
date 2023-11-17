e471736b8c5656b61ff231f010eef6874f50b7ed
vkusnyipecheniya <150159157+vkusnyipecheniya@users.noreply.github.com>
2023-11-17 22:18:09 +0600

    Add files via upload
    only app

--- app.js
+++ app.js
@@ -1,42 +1,8 @@
-// console.log('hello')
-
-//флаги
-
-
-//регулярные выражения - regular expression
-
-
-//
-// const string = prompt('Enter your name...')
-//
-// const regExp = /n/gi
-//
-// console.log(string.match(regExp));
-//
-
-
-// const symbol='AaabbbCcc'
-// const regExp =/c/gi
-//
-// console.log(symbols.match(regExp));
-
-// const numbers='123Qwe123_qwe_213'
-
-// const regExp = new RegExp('123','g')
-// const regExp=/[\d&?]/g
-
-// console.log(numbers.replace(regExp, '*'))
-// console.log(numbers.match(regExp))
-
-//Рекурсия - Recursion
-
-let num = 0
-
-const count = () => {
-    num++
-    console.log(num)
-    if(num<500){
-        count()
-    }
-}
-count()
+fetch('https://jsonplaceholder.typicode.com/posts')
+  .then(response => response.json()) 
+  .then(data => {
+    console.log(data); 
+  })
+  .catch(error => {
+    console.error('Ошибка:', error);
+  });
